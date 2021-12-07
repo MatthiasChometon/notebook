ce701ad25c41bd7d07f9c1bfb0b305263cc6fd22
MatthiasChometon <matthiaschometon787@gmail.com>
2021-12-07 21:13:03 +0100

    feat: change style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,16 @@
 <template>
   <v-app light>
+    <AppBar />
     <v-main>
-      <v-container>
+      <v-container class="d-flex justify-start align-start container mt-8">
         <Nuxt />
       </v-container>
     </v-main>
   </v-app>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
-  },
-}
-</script>
+<style scoped>
+  .container {
+    height: 95vh;
+  }
+</style>
