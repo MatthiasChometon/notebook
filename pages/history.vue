f57d7d6cc20789894913789e8d829023f9d8a6f9
MatthiasChometon <matthiaschometon787@gmail.com>
2021-12-07 21:14:18 +0100

    feat: add history page

---
+++ pages/history.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-alert
+      v-for="(historyLine, index) in $store.getters['getHistory']"
+      :key="index"
+      outlined
+      :color="historyLine.color"
+      >{{ historyLine.description }}</v-alert
+    >
+    <v-alert
+      v-if="$store.getters['getHistory'].length === 0"
+      border="top"
+      color="red lighten-3"
+      dark
+    >
+      no history
+    </v-alert>
+  </div>
+</template>
