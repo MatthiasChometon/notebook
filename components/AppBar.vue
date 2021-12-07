e935e5962788da076dc0b770557d4a8773df4ae4
MatthiasChometon <matthiaschometon787@gmail.com>
2021-12-07 21:13:36 +0100

    feat: add AppBar component

---
+++ components/AppBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-app-bar color="teal lighten-3" dense>
+      <v-toolbar-title class="white--text">Notebook</v-toolbar-title>
+
+      <NuxtLink class="nuxtlink white--text" to="/">in progress</NuxtLink>
+      <NuxtLink class="nuxtlink white--text" to="/done">done</NuxtLink>
+      <NuxtLink class="nuxtlink white--text" to="/history">history </NuxtLink>
+    </v-app-bar>
+  </div>
+</template>
+
+<style scoped>
+.nuxtlink {
+  text-decoration: none;
+  margin: 2%;
+}
+</style>
