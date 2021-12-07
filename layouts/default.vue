d8a12f95a80b538ade2d0b61f0b20dd0e4e78142
MatthiasChometon <matthiaschometon787@gmail.com>
2021-12-07 21:39:42 +0100

    feat: delete useless style

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,9 @@
   <v-app light>
     <AppBar />
     <v-main>
-      <v-container class="d-flex justify-start align-start container mt-8">
+      <v-container class="d-flex justify-start align-start mt-8">
         <Nuxt />
       </v-container>
     </v-main>
   </v-app>
 </template>
-
-<style scoped>
-  .container {
-    height: 95vh;
-  }
-</style>
